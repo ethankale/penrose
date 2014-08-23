02bc129e64fdd8d8392d246268a6ac66281dcc5a
lynnkale <lynnkale+dailypushupclub@gmail.com>
2014-08-23 13:40:56 -0500

    Added vertex groups and some related logic

--- scripts/penrose.js
+++ scripts/penrose.js
@@ -20,6 +20,7 @@
             startY: 200,
         };
         var _canvas;
+        var _makingTiles;
 
         //----------------------------------------------------------------------
         // Private Jquery Objects
@@ -49,25 +50,60 @@
         };
 
         this.startTiling = function() {
+
             var firstTile = new Tile({
                 x0: _settings.startX,
-                y0: _settings.startY
+                y0: _settings.startY,
+                shape: 'dart'
             });
             firstTile.draw(_canvas);
 
-            tiles.push(firstTile);
+            // tiles.push(firstTile);
+            var index;
+            var vg;
             for (var i in firstTile.points) {
-                var index = firstTile.points[i][0].toFixed(3) + '_' + firstTile.points[i][1].toFixed(3);
-                vertices[index] = {
-                    'tile': firstTile,
-                    'point': firstTile.pointNames[i]
+                index = firstTile.points[i][0].toFixed(3) + '_' + firstTile.points[i][1].toFixed(3);
+                if (!vertices[index]) {
+                    vg = new VertexGroup({x: firstTile.points[i][0], y: firstTile.points[i][1]});
+                    vertices[index] = vg;
+                }
+                vertices[index].addVertex(firstTile.pointNames[i], firstTile);
+            }
+            var secondTile = new Tile({
+                x0: _settings.startX,
+                y0: _settings.startY,
+                shape: 'kite'
+            });
+            secondTile.draw(_canvas);
+            for (var i in secondTile.points) {
+                index = secondTile.points[i][0].toFixed(3) + '_' + secondTile.points[i][1].toFixed(3);
+                if (!vertices[index]) {
+                    vg = new VertexGroup({x: secondTile.points[i][0], y: secondTile.points[i][1]});
+                    vertices[index] = vg;
+                }
+                vertices[index].addVertex(secondTile.pointNames[i], secondTile);
+            }
+            
+            console.log(vertices);
+
+            // _makingTiles = setInterval(addTile, 1000);
+            
+        }
+
+        addTile = function() {
+            console.log('adding a tile');
+            for (var i in vertices) {
+                var vg = vertices[i];
+                if (!vg.isComplete()) {
+                    console.log(vg);
+                    window.vg = vg;
+                    vg.checkComplete();
                 }
             }
-            console.log(vertices);
         }
 
         this.stopTiling = function() {
-            clearInterval(makingTiles);
+            clearInterval(_makingTiles);
         }
     
     };
@@ -97,8 +133,8 @@
             colors: {
                 fill:   '#cccccc',
                 stroke: '#0000ff',
-                arc1:   '#ffff00',
-                arc2:   '#800080',
+                arc1:   '#42B364',
+                arc2:   '#C10313',
             }
         };
         var points = [];
@@ -112,9 +148,16 @@
         this.init = function () {
             // Combine our options and our defaults
             _settings = $.extend(true, {}, _defaults, options);
+            if (!options.colors || !options.colors.fill) {
+                if (_settings.shape == 'kite') {
+                    _settings.colors.fill = '#F2F2F2';
+                } else {
+                    _settings.colors.fill = '#E0E0E0';
+                }
+            }
             console.log('Tile settings');
             console.log(_settings);
-            // A / E
+            // A / H
             this.x0       = _settings.x0;
             this.y0       = _settings.y0;
             this.shape    = _settings.shape;
@@ -126,29 +169,34 @@
             this.antiAngle = this.rotation - 36;
 
             if (this.shape == "kite") {
-                // F
+                // B
                 this.x1 = this.x0 + (this.length * Math.cos(Geometry.degToRad(this.antiAngle)));
                 this.y1 = this.y0 + (this.length * Math.sin(Geometry.degToRad(this.antiAngle)));
-                // G
+                // C
                 this.x2 = this.x0 + (this.length * Math.cos(Geometry.degToRad(this.rotation)));
                 this.y2 = this.y0 + (this.length * Math.sin(Geometry.degToRad(this.rotation)));
-                // H
+                // D
                 this.x3 = this.x0 + (this.length * Math.cos(Geometry.degToRad(this.angle)));
                 this.y3 = this.y0 + (this.length * Math.sin(Geometry.degToRad(this.angle)));
-
-                this.pointNames = ['E','F','G','H'];
-            } else if (this.shape == "dart") {
-                // B
-                this.x1 = this.x0 + (this.length * Math.cos(Geometry.degToRad(this.angle)));
-                this.y1 = this.y0 + (this.length * Math.sin(Geometry.degToRad(this.angle)));
-                // C
-                this.x2 = this.x1 - (this.length * Math.sin(Geometry.degToRad(54 + this.rotation)));
-                this.y2 = this.y1 + (this.length * Math.cos(Geometry.degToRad(54 + this.rotation)));
-                // D
-                this.x3 = this.x2 + (this.shortLen * Math.sin(Geometry.degToRad(18 + this.rotation)));
-                this.y3 = this.y2 - (this.shortLen * Math.cos(Geometry.degToRad(18 + this.rotation)));
+                // console.log('B', this.x1, this.y1);
+                // console.log('C', this.x2, this.y2);
+                // console.log('D', this.x3, this.y3);
 
                 this.pointNames = ['A','B','C','D'];
+            } else if (this.shape == "dart") {
+                // E
+                this.x1 = this.x0 + (this.length * Math.cos(Geometry.degToRad(this.angle)));
+                this.y1 = this.y0 + (this.length * Math.sin(Geometry.degToRad(this.angle)));
+                // F
+                this.x2 = this.x1 - (this.length * Math.sin(Geometry.degToRad(54 + this.rotation)));
+                this.y2 = this.y1 + (this.length * Math.cos(Geometry.degToRad(54 + this.rotation)));
+                // G
+                this.x3 = this.x2 + (this.shortLen * Math.sin(Geometry.degToRad(18 + this.rotation)));
+                this.y3 = this.y2 - (this.shortLen * Math.cos(Geometry.degToRad(18 + this.rotation)));
+                // console.log('E', this.x1, this.y1);
+                console.log('F', this.x2, this.y2);
+
+                this.pointNames = ['H','E','F','G'];
             }
 
             this.points = [[this.x0, this.y0], [this.x1, this.y1], [this.x2, this.y2], [this.x3, this.y3]];
@@ -215,8 +263,156 @@
 
         this.init(options);
     }
+
+    var VertexGroup;
+    VertexGroup = function(options) {
+
+        //----------------------------------------------------------------------
+        // Private Properties
+        //----------------------------------------------------------------------
+        var _settings = {};
+        var _defaults = {
+            x: 0,
+            y: 0
+        };
+        var _allGroups = [
+            'EEEEE',
+            'GBD',
+            'AAAAA',
+            'EEDBE',
+            'CFAAH',
+            'EDBDB',
+            'HCCF'
+        ];
+        var _possibleGroups = [];
+
+        var _matchStrings = [
+            'EEEEE',
+            'GBDGB',
+            'AAAAA',
+            'EEDBEEEDB',
+            'CFAAHCFAA',
+            'EDBDBEDBD',
+            'HCCFHCC'
+        ];
+
+        var _rotations = [
+            [126, 198, 270, 342, 54],
+            [270, 306, 234],
+            [270, 342, 54, 126, 198],
+            [90, 162, 162, 18, 18],
+            [90, 54, 54, 126, 126],
+            [90, 90, 306, 234, 90],
+            [234, 198, 342, 306]
+        ];
+
+        var vertices = [];
+        this.tiles    = [];
+
+        var _complete = false;
+
+        /**
+         * This will initialize the unset private variables and create the
+         * manager object.
+         *
+         * @return void
+         */
+        this.init = function () {
+            _settings = $.extend(true, {}, _defaults, options);
+            // for debugging
+            this.x = _settings.x;
+            this.y = _settings.y;
+        }
+
+        this.isComplete = function () {
+            return _complete;
+        }
+
+        /**
+         * @param string vName alphabetic vertex identifier (e.g. 'E')
+         * @param Tile tile the tile whose vertex is at this point
+         * @return void
+         */
+        this.addVertex = function(vName, tile) {
+            vertices.push(vName);
+            this.tiles.push(tile);
+        }
+
+        this.checkComplete = function() {
+            /*
+             * check 360 degrees around the point and figure out what type of group it is, or at
+             * least eliminate impossible groups.
+             * draw a circle of a radius much shorter than the size of the polygon
+             */
+            var radius = this.tiles[0].length * 0.1;
+            // console.log('radius');
+            // console.log(radius);
+            // console.log( _settings.x, _settings.y);
+            var rotation = 360
+            var pointX, pointY;
+            var lastTile;
+            var orderedTiles = [];
+            var matchString = '';
+            var gaps = false;
+            while ( rotation > 0) {
+                var rotationInRadians = Geometry.degToRad(rotation);
+                pointX = _settings.x + (radius * Math.sin(rotationInRadians));
+                pointY = _settings.y + (radius * Math.cos(rotationInRadians));
+// console.log(pointX, pointY, rotation);
+
+                var found = false;
+                for( var i in this.tiles) {
+                    if (Geometry.pointInsidePoly([pointX, pointY], this.tiles[i].points)) {
+// console.log('point is in tile ' + i + ', ' + vertices[i]);
+                        if (this.tiles[i] != lastTile) {
+                            lastTile = this.tiles[i];
+                            if (orderedTiles.indexOf(this.tiles[i]) < 0) {
+                                matchString += vertices[i];
+                                orderedTiles.push(this.tiles[i]);
+                            }
+                        }
+                        found = true;
+                    }
+                }
+                if (!found && lastTile != '.') {
+                    lastTile = '.';
+                    matchString += '.+';
+                    gaps = true;
+                }
+                rotation -= 35;
+            }
+            console.log(matchString);
+            console.log(orderedTiles);
+            if (!gaps) {
+                _complete = true;
+            }
+
+            // check which patterns match our possible pattern list
+            var pattern = new RegExp(matchString);
+            console.log('pattern', pattern);
+            _possibleGroups = [];
+            for (var i = 0; i < 7; i++) {
+                if (_matchStrings[i].match(pattern)) {
+                    _possibleGroups.push(i);
+                }
+            }
+            console.log(_allGroups);
+            console.log(_possibleGroups);
+
+            // pick a random pattern option
+            var useGroupIndex = _possibleGroups[Math.floor(Math.random()*_possibleGroups.length)];
+            console.log('use ' + _allGroups[useGroupIndex]);
+            
+            // determine which tiles are missing from the vertex group, and the rotation of the group
+            
+        }
+
+        this.init(options);
+    }
+
     window.Penrose = Penrose;
     window.Tile = Tile;
+
 }(jQuery));
 
     
--- scripts/point-in-polygon.js
+++ scripts/point-in-polygon.js
@@ -1,4 +1,5 @@
-﻿var Geometry = Geometry || {};
+﻿// acquired and modified from https://github.com/substack/point-in-polygon
+var Geometry = Geometry || {};
 Geometry.pointInsidePoly = function (point, vs) {
     // ray-casting algorithm based on
     // http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
